e8609818c10440d1d6828616b8daa890f0303748
Ryan Morash <ryan@morash.me>
2024-05-10 23:26:39 -0400

    Initial Commit

---
+++ Whenplane/WhenplaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhenplaneApp.swift
+//  Whenplane
+//
+//  Created by Ryan Morash on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WhenplaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
